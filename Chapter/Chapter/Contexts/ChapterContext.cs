c1f5d9d3c8f8d6c61a3a4f80869d24727f9677af
Caique Zaneti Kirilo <caiquez.kirilo@hotmail.com>
2023-08-09 19:47:01 -0300

    ER1

---
+++ Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,25 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext() { }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options) { }
+        //vamos utilizar esse método par configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) 
+            {
+                //cada provedor tem sua sintaxe para especificação
+                // optionsBuilder.UseSqlServer("Data Source = DESKTOP-F2ABGDE\\SQLEXPRESS; initial catalog = Chapter2 ; user id = seuuser; password = suasenha ;");
+                // TrustServerCertificate
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-F2ABGDE\\SQLEXPRESS; initial catalog = Chapter2 ; Integrated Security = true ; TrustServerCertificate = True");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
--- Chapter/Chapter/Models/Livro.cs
+++ Chapter/Chapter/Models/Livro.cs
@@ -2,5 +2,9 @@
 {
     public class Livro
     {
+        public int Id { get; set; } 
+        public string? Titulo { get; set; }
+        public int QuantidadePaginas { get; set; }
+        public bool Disponivel { get; set; }
     }
 }
