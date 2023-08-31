025378920af2af9d674ffe96dde00cccd92e18ee
Caique Zaneti Kirilo <caiquez.kirilo@hotmail.com>
2023-08-31 19:48:36 -0300

    ER4

--- Chapter/Chapter/Program.cs
+++ Chapter/Chapter/Program.cs
@@ -1,6 +1,7 @@
 using Chapter.Contexts;
 using Chapter.Interfaces;
 using Chapter.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,24 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBearer";
+    options.DefaultAuthenticateScheme = "JwtBearer";
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(60),
+        ValidIssuer = "chapter.webapi",
+        ValidAudience = "chapter.webapi"
+    };
+}
+);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -35,6 +54,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ Chapter/Chapter/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chapter.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "E-mail necessario")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha necessaria")]
+        public string Senha { get; set; }
+    }
+}
